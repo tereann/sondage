aa09d123b15ba09aab89a34dfaf235d4b4644166
tereann <121940590+tereann@users.noreply.github.com>
2023-04-21 12:48:44 +0200

    all tests + readme

---
+++ src/test/java/fr/simplon/sondage/README.md
@@ -0,0 +1,12 @@
+# Survey Web Service Project
+
+This is a web service project that is designed to eventually make a survey. 
+Currently, the project only has one entity class called "Sondage", which represents a survey.
+
+The project includes JUnit tests to test each interaction with the Sondage entity, 
+including creating a new Sondage, updating an existing Sondage, getting a list of all Sondages, 
+getting one particular Sondage using id 
+and deleting a Sondage.
+
+Future development of this project will include additional entity classes, 
+as well as the ability to create, respond to, and analyze surveys.
--- src/test/java/fr/simplon/sondage/SondageApplicationTests.java
+++ src/test/java/fr/simplon/sondage/SondageApplicationTests.java
@@ -1,16 +1,12 @@
 package fr.simplon.sondage;
 
+
 import fr.simplon.sondage.entity.Sondage;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDate;
@@ -19,24 +15,27 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+
 @SpringBootTest
 class SondageApplicationTests {
 
+    private RestTemplate restTemplate = new RestTemplate();
+    private HttpHeaders headers = new HttpHeaders();
+
+
     @Test
     void testGetAllSondages() {
-        RestTemplate restTemplate = new RestTemplate();
         String url = "http://localhost:8080/sondages";
         ResponseEntity<List<Sondage>> response = restTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<List<Sondage>>() {
         });
         List<Sondage> sondages = response.getBody();
 
-        assertEquals(4, sondages.size());
+        assertEquals(sondages.stream().count(), sondages.size());
     }
 
     @Test
     void testGetSondage() {
-        long id = 1L;
-        RestTemplate restTemplate = new RestTemplate();
+        long id = 11L;
         String url = "http://localhost:8080/sondages/1";
         ResponseEntity<Sondage> response = restTemplate.getForEntity(url, Sondage.class, id);
         Sondage sondage = response.getBody();
@@ -48,11 +47,9 @@ class SondageApplicationTests {
 
     @Test
     void testAddSondage() {
-        RestTemplate restTemplate = new RestTemplate();
-        Sondage sondage = new Sondage("test", "test", LocalDate.now(), LocalDate.of(2023, 4, 27), "Anonyme");
+        Sondage sondage = new Sondage("TESTING", "test", LocalDate.now(), LocalDate.of(2023, 4, 27), "Anonyme");
 
         String url = "http://localhost:8080/sondages";
-        HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<Sondage> request = new HttpEntity<>(sondage, headers);
         ResponseEntity<Sondage> response = restTemplate.postForEntity(url, request, Sondage.class);
@@ -64,5 +61,90 @@ class SondageApplicationTests {
         assertEquals(sondage.getDate_creation(), response.getBody().getDate_creation());
         assertEquals(sondage.getEnd_date(), response.getBody().getEnd_date());
         assertEquals(sondage.getNom_creator(), response.getBody().getNom_creator());
+
+        //To delete the added
+        restTemplate.delete(url + "/" + response.getBody().getId());
     }
+
+    //THIS ONE ACTUALLY DELETES THE FILE FROM DB
+   /* @Test
+    void testDelSondage() {
+        long id = 10L;
+        String url = "http://localhost:8080/sondages/{id}";
+        ResponseEntity<Sondage> response = restTemplate.getForEntity(url, Sondage.class, id);
+        restTemplate.delete(url, id);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    } */
+
+
+    @Test
+    void testUpdateSondage() {
+        Sondage updatedSondage = new Sondage();
+        Sondage originalSondage;
+        long id = 14L;
+        String url = "http://localhost:8080/sondages/" + id;
+        ResponseEntity<Sondage> response = restTemplate.getForEntity(url, Sondage.class, id);
+        originalSondage = response.getBody();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        updatedSondage.setDescription("Pot-Luck");
+        updatedSondage.setQuestion("Do you want to bring food to share on Tuesday?");
+        updatedSondage.setDate_creation(LocalDate.now());
+        updatedSondage.setEnd_date(LocalDate.of(2023, 4, 28));
+        updatedSondage.setNom_creator("Test");
+
+        restTemplate.put(url, updatedSondage);
+
+        ResponseEntity<Sondage> updatedResponse = restTemplate.getForEntity(url, Sondage.class);
+
+        assertNotNull(updatedResponse);
+        assertNotNull(updatedResponse.getBody());
+        assertEquals(HttpStatus.OK, updatedResponse.getStatusCode());
+        assertNotNull(updatedResponse.getBody().getId());
+        assertEquals(updatedSondage.getQuestion(), updatedResponse.getBody().getQuestion());
+        assertEquals(updatedSondage.getDescription(), updatedResponse.getBody().getDescription());
+        assertEquals(updatedSondage.getDate_creation(), updatedResponse.getBody().getDate_creation());
+        assertEquals(updatedSondage.getEnd_date(), updatedResponse.getBody().getEnd_date());
+        assertEquals(updatedSondage.getNom_creator(), updatedResponse.getBody().getNom_creator());
+
+        //TO PUT BACK ORIGINAL CONTENT
+        restTemplate.put(url, originalSondage);
+        ResponseEntity<Sondage> original = restTemplate.getForEntity(url, Sondage.class);
+    }
+
+    @Test
+    void testDeleteSondage() {
+        long id = 11L;
+        String url = "http://localhost:8080/sondages/{id}";
+
+        // Mock the restTemplate
+        RestTemplate restTemplateMock = Mockito.mock(RestTemplate.class);
+
+        // Create a sample response entity
+        Sondage sondage = new Sondage("Question", "Description", LocalDate.now(), LocalDate.of(2023, 4, 28), "Creator");
+        ResponseEntity<Sondage> responseEntity = new ResponseEntity<>(sondage, HttpStatus.OK);
+
+        // When the restTemplate gets the entity, return the sample response entity
+        Mockito.when(restTemplateMock.getForEntity(url, Sondage.class, id)).thenReturn(responseEntity);
+
+        // Create a sample response entity for the delete request
+        ResponseEntity<Void> deleteResponseEntity = new ResponseEntity<>(HttpStatus.OK);
+
+        // When the restTemplate deletes the entity, return the sample response entity
+        Mockito.when(restTemplateMock.exchange(url, HttpMethod.DELETE, null, Void.class, id)).thenReturn(deleteResponseEntity);
+
+        // Call the method that uses the restTemplate
+        ResponseEntity<Sondage> response = restTemplateMock.getForEntity(url, Sondage.class, id);
+        restTemplateMock.exchange(url, HttpMethod.DELETE, null, Void.class, id);
+
+        // Assert that the response entity has a HTTP status code of OK
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+
 }
+
+
+
+
